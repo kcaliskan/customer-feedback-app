470c90924436d275f5515fe060c54ff56e358998
Kaan Caliskan <kaancaliskan@Kaans-MacBook-Air.local>
2019-02-05 16:49:57 -0800

    added mongodb and user login/logout

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,8 @@
 module.exports = {
   googleClientID:
     "160450491876-sugefnutb1h43s2qthgppilqemqqdsts.apps.googleusercontent.com",
-  googleClientSecret: "50lF1nxwsR2yMXch4DSwGQF4"
+  googleClientSecret: "50lF1nxwsR2yMXch4DSwGQF4",
+  mongoURI:
+    "mongodb://emailydev:senveben123@ds119765.mlab.com:19765/emaily-dev",
+  cookieKey: "12h3jk21h489uasodiya8s7djn3l246ihfjadlk761e2v3anzxkowiodasj"
 };
