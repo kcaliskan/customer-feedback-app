e8bfb2dc8b37f7d9012fb37ac4b1f2eb80e5700a
Kaan Caliskan <mail@kcaliskan.com>
2019-02-07 00:45:36 -0800

    re-organized config folder structure

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,4 @@
 //key.js - figure out what set of credentials to return
-
 if (process.env.NODE_ENV === "production") {
   // we are in production = return the prod set of keys
   module.exports = require("./prod");
