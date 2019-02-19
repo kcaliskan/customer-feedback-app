28f9ea565af76a2665be211c030956b79ba0acb7
Kaan Caliskan <mail@kcaliskan.com>
2019-02-19 10:36:45 -0800

    Mailer constructur and boilerpalte for sending emails

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -2,6 +2,46 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.form_email = new helper.Email("no-reply@upvote.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    //This addContent was a function that was provided by the base class (helper.Mail)
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    //We're taking in the list of recipients which is our sub document collection. We pass it off to format addresses for every recipient inside that array. We pull off the e-mail property we format it with the email helper and then we return it. we make ([email: 'email@address.com']) to (['email@address.com'])
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addClickTrackings(trackingSettings);
+  }
+
+  addRecipients() {
+    //So for each one of those take them and add them to the personalized object. And then after they've all been added to the personalized object call this.addPersonalization() which is that function that is defined by the male base class and add the entire personalized object.
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+
+    this.addPersonalization(personalize);
+  }
+}
 
 module.exports = Mailer;
