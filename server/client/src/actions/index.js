2666c7329a3cb8e9ac237164ebf4ce7acc6d5c86
Kaan Caliskan <mail@kcaliskan.com>
2019-02-24 17:48:17 -0800

    added survey list dashboard

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
+import { FETCH_SURVEYS } from "./types";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
@@ -17,4 +18,10 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,8 +10,12 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
-    res.send("Thanks for voting!");
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
